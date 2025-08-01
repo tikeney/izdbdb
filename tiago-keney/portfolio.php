ae75bf2279704c18b632dab2a1d96766b1ddf792
tikeney <tikeney33@gmail.com>
2025-08-01 14:41:30 -0300

    adicionando arquivos principais

---
+++ tiago-keney/portfolio.php
@@ -0,0 +1,34 @@
+<?php
+
+// Lê o arquivo JSON
+$dadosJson = file_get_contents("curriculo.json");
+$osd = json_decode($dadosJson, true);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="redirectionPortifolio"><a href="portifolio.php">Portfolio</a></div>
+        <div class="redirectionCurriculo"><a href="curriculo.php">Curriculo</a></div>
+    </div>
+    <div class="main">
+        
+        <div class="person"></div>
+        <div class="text"></div>
+
+    </div>
+</body>
+
+</html>
