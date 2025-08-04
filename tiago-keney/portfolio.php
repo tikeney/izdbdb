234a64f85bd3fce178fb3d6f0e5e2d5c51f7cd60
tikeney <tikeney33@gmail.com>
2025-08-04 14:15:09 -0300

    31-07-25

--- tiago-keney/portfolio.php
+++ tiago-keney/portfolio.php
@@ -16,10 +16,16 @@ $osd = json_decode($dadosJson, true);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+</head>
 </head>
 
 <body>
     <div class="header">
+        <div class="redirectionHome"><a href="portifolio.php">Home</a></div>
         <div class="redirectionPortifolio"><a href="portifolio.php">Portfolio</a></div>
         <div class="redirectionCurriculo"><a href="curriculo.php">Curriculo</a></div>
     </div>
