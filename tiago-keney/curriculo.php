971827682e51137722e5a380b795c0870eb8bcd2
tikeney <tikeney33@gmail.com>
2025-08-08 20:23:15 -0300

    começo da pagina de curriculo

---
+++ tiago-keney/curriculo.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Curriculo/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="links">
+            <a href="index.html" class="headerRedirection">Portfolio</a>
+        </div>
+    </div>
+    <div class="main">
+        <div class="principal">
+            
+        </div>
+    </div>
+</body>
+
+</html>
